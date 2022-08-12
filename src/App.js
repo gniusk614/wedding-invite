c268cd0ec62237cc24601dc5e1ddc16a889218a0
naver <gniusk614@nate.com>
2022-08-12 16:28:54 +0900

    교통편 입력 완료

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import * as React from "react";
 import FixedBottomNavigation from "./component/bottom-nav";
 import Home from "./component/home";
 import video2 from "./lib/img/video_2.mov";
-
+import { Helmet } from "react-helmet-async";
 function App() {
   function setScreenSize() {
     let vh = window.innerHeight * 0.01;
@@ -16,6 +16,12 @@ function App() {
 
   return (
     <div className="App">
+      <Helmet>
+        <title>광훈♡희연 결혼식에 초대합니다.</title>
+        <meta property="og:title" content="광훈♡희연 결혼식에 초대합니다."></meta>
+        <meta property="og:image" content="./lib/img/gallary/pic_8.jpg"></meta>
+        <meta property="og:description" content="2022.10.08(토) 오전 11시 더리버사이드호텔 LL층 노벨라홀" />
+      </Helmet>
       <Home></Home>
       <FixedBottomNavigation />
     </div>
